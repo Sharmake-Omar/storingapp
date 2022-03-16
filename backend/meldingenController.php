352febe69883068f5c8a5c4fa712be43cd762dda
Sharmake-Omar <93530529+Sharmake-Omar@users.noreply.github.com>
2022-03-16 13:43:22 +0100

    insert quary gemaakt 5 stappen

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -11,7 +11,14 @@ echo $attractie . " / " . $capaciteit . " / " . $melder;
 require_once 'conn.php';
 
 //2. Query
-
+$query = "INSERT INTO meldingen (attractie, type)
+VALUES(:attractie, :type)";
 //3. Prepare
-
+$statement = $conn->prepare($query);
 //4. Execute
+$statement->execute([
+    ":attractie" => $attractie,
+    ":type" => $type,
+   ]);
+
+   $items = $statement->fetchAll(PDO::FETCH_ASSOC);
