94b7aa09492946654fd2a5678b802fcd436aa771
Sharmake-Omar <93530529+Sharmake-Omar@users.noreply.github.com>
2022-03-17 08:56:35 +0100

    Form toegevoegd

--- meldingen/create.php
+++ meldingen/create.php
@@ -21,17 +21,30 @@
             </div>
             <div class="form-group">
                 <label for="type">Type</label>
-                <!-- hier komt een dropdown -->
+                <select name="type_" id="groep">
+                    <option Value=""> - Kies een type - </option>
+                    <option Value=""> - KinderAttractie - </options>
+                    <option Value=""> - WaterAchtbaan - </options>
+                    <option Value=""> - Horeca,Cafe,Overige - </options>
+                    <option Value=""> - DraaiendAttractie - </options>
+            </select>
             </div>
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
             </div>
             <div class="form-group">
+                <label for="prioriteit">Prio:</label>
+                <input type="checkbox" name="prioriteit" id="prioriteit">
+                <label for="prioriteit">Melding met Prioriteit</label>
+            <div class="form-group">
                 <label for="melder">Naam melder:</label>
                 <input type="text" name="melder" id="melder" class="form-input">
             </div>
-            
+            <div class="form-group">
+                <label for="overige_info">overige info:</label>
+                <textarea name="overige_info" id="overig" class="form-input" rows="4"></textarea>
+            </div>
             <input type="submit" value="Verstuur melding">
 
         </form>
